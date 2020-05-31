753fc1972eaf4229c83eb33c285a496ed9b396cf
Miguel <miguel_angel_bucio@hotmail.com>
2020-05-31 18:05:34 +0000

    Practica 13

--- practica13/replace.c
+++ practica13/replace.c
@@ -1,44 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int main(int argn, char **argv){
-    char *filename = argv[3];
+void replace_word(int tmp, char *word,char *searchWord,char *replaceWord, char *character);
+
+int main(int argn,char **argv){
+    char *fileName = argv[3];
     char *searchWord = argv[1];
     char *replaceWord = argv[2];
+    char *tmpFileName = "/tmp/replace.txt";
+    char *tmpnewFile = "/tmp/new_file.txt";
     char word[100];
-    char character;
-    char espacio = 0;
     int i = 0;
-    int fd = open(filename, O_RDONLY);
-
-    if (fd<0){
-        printf("No se puede abrir el archivo");
+    char character;
+    int fd = open(fileName, O_RDONLY);
+    int tmp = open(tmpFileName,O_WRONLY|O_CREAT,0600);
+    if(fd < 0) {
+        printf("No se puede abrir \n");
         exit(1);
     }
-
-    while (read(fd, &character, 1)) {
-        if ((character == ' ' || character == '\n' ) && espacio ==0 ){
-            espacio = 1;
+    while(read(fd,&character,1)){
+        if((character == ' ' || character == '\n')) {
+            word[i++] = character;
             word[i] = '\0';
             i = 0;
-            if (strcmp(word, searchWord)==0){
-                printf ("%s \n", replaceWord);
-            }else {
-                printf("%s \n", word);
-            }
+            replace_word(tmp, word, searchWord, replaceWord, &character);
+        } else if(character != ' ' && character != '\n') {
+        word[i++] = character;
         }
-        else if (character != ' ' && character != '\n' ){
-            espacio = 0;
-            word[i++] = character;
-        }
-       
     }
-
+    word[i] = '\0';
+    character = 0x05; //END OF FILE
+    replace_word(tmp, word, searchWord, replaceWord, &character);
     close(fd);
+    close(tmp);
+    //Renombrar archivo
+    unlink(fileName);   //Borrar original
+    //link(tmpFileName, fileName);    //Crear Alias
+    link(tmpFileName, tmpnewFile);    //Crear archivo en tmp porque no se puede en workspace
+    unlink(tmpFileName);    //Borrar temporal
     exit(0);
+}
+
+
+void replace_word(int tmp, char *word,char *searchWord,char *replaceWord, char *character){
+    if( strncmp(word,searchWord,strlen(searchWord)) == 0){
+        write(tmp,replaceWord,strlen(replaceWord));
+        write(tmp,character, 1);
+    } else {
+        write(tmp,word,strlen(word));
+    }
 }
